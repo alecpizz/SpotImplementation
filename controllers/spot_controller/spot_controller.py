936efe81f301ab215c892fc0183ef05bce683ddc
Alec Pizziferro <alecpizz@gmail.com>
2025-02-10 20:23:31 -0600

    turn right if there is a wall

--- controllers/spot_controller/spot_controller.py
+++ controllers/spot_controller/spot_controller.py
@@ -2,7 +2,13 @@
 from controller import AnsiCodes
 from spot_driver import SpotDriver
 import numpy as np
+from enum import Enum
 spot = SpotDriver()
+
+class Direction(Enum):
+    Forward = 0
+    Left = 1
+    Right = 2
 
 FRONT_DISTANCE = 0.27
 #TODO: Figure out thresholds. will probably need to be setup based on the size of the maze.
@@ -14,6 +20,9 @@ FRONT_WALL_INDEX = 3
 left_wall = False
 right_wall = False
 front_wall = False
+left_wall_prev = False
+right_wall_prev = False
+front_wall_prev = False
 sectors = []
 
 def check_walls():
@@ -22,24 +31,34 @@ def check_walls():
     front_wall_present()
 
 def left_wall_present():
-    global left_wall, sectors
+    global left_wall, left_wall_prev, sectors
+    left_wall_prev = left_wall
     current_left = np.mean(sectors[LEFT_WALL_INDEX])
     print("Left Wall diff at sector:", LEFT_WALL_INDEX,  current_left)
     left_wall = WALL_MIN_THRESHOLD <= current_left <= WALL_MAX_THRESHOLD
 
 def right_wall_present():
-    global right_wall, sectors
+    global right_wall, sectors, right_wall_prev, sectors
+    right_wall_prev = right_wall
     current_right = np.mean(sectors[RIGHT_WALL_INDEX])
     print("Right Wall diff at sector:", RIGHT_WALL_INDEX, current_right)
     right_wall = WALL_MIN_THRESHOLD <= current_right <= WALL_MAX_THRESHOLD
 
 def front_wall_present():
-    global front_wall, sectors
+    global front_wall, front_wall_prev, sectors
+    front_wall_prev = front_wall
     current_front = np.mean(sectors[FRONT_WALL_INDEX])
     print("Front Wall diff at sector:", FRONT_WALL_INDEX, current_front)
     front_wall = WALL_MIN_THRESHOLD <= current_front <= WALL_MAX_THRESHOLD
 
+def right_wall_changed():
+    return right_wall_prev != right_wall
 
+def left_wall_changed():
+    return left_wall_prev != left_wall
+
+def front_wall_changed():
+    return front_wall_prev != front_wall
 
 def is_front(value):
     return value > FRONT_DISTANCE
@@ -54,18 +73,14 @@ def wall_direction( lidar_data, num_sectors=8):
     #split the data into 8 sectors, store em somewhere
 
 while spot.step(spot.get_timestep()) != -1:
-    print(AnsiCodes.CLEAR_SCREEN + "\n")
+    print(AnsiCodes.CLEAR_SCREEN + "\nDATA:")
     lidar = np.array(spot.get_lidar_image())
     lidar = lidar[np.isfinite(lidar)]
     wall_direction(lidar)
     check_walls()
-    if left_wall:
-        print("Left Wall detected")
-    elif right_wall:
-        print("Right Wall detected")
-    elif front_wall:
-        print("Front Wall detected")
+    if not right_wall:
+        print("wheres my fuckin right wall!")
+        # turn or something
+        spot.turnright(100)
     else:
-        print("No Wall detected")
-
-
+        spot.forward(15)
